adf15fe8226709dbd7f5ff81e7dcb5db943113dd
Swapnil Patil <patilswapnil01sp.25@gmail.com>
2022-03-15 23:56:51 +0530

    Data from github

--- All-Projects/reviews/src/index.js
+++ All-Projects/reviews/src/index.js
@@ -2,23 +2,38 @@
 const url = 'https://api.github.com/repos/elkorf/JavaScript-Projects/contents/All-Projects/reviews/review-data';
 let reviewText = document.querySelector('#review-txt');
 let prev = document.querySelector('.prev');
-let next = document.querySelector('.next'); 
-let result="";
+let next = document.querySelector('.next');
+let reviewProfile = document.querySelector('#reviewer-profile');
+let reviewerName = document.querySelector('#reviewer-name'); 
+let result;
 let counter=0;
-
+let reviewer_img="";
+let img_str = "./review-data/reviewer-profile/";
 window.addEventListener("DOMContentLoaded", async function (e) {
   try {
     const response = await fetch(url);
     result = await response.json();
     reviewText.innerText =  (await (await fetch(result[0].download_url)).text());;
+    // console.log(result);
+    let chk = img_str+result[0].name;
+    reviewProfile.src = chk.substring(0, chk.lastIndexOf(".")) + ".jpg";
+
+    Name = result[0].name;
+    // reviewerName.innerText = Name.substring(0, Name.lastIndexOf(".")) + "";
   } catch (err) {
     return console.log(err);
   }
 });
 
 next.onclick = async () =>{
-  reviewText.innerText =  (await (await fetch(result[(++counter)%4].download_url)).text());
-  // console.log((++counter)%4);
+  reviewText.innerText = (await (await fetch(result[(++counter)%4].download_url)).text());
+  if(result[4].type=="dir"){
+    // console.log(result[4].path+"/");
+    let chk = img_str+result[(counter)%4].name;
+    reviewProfile.src = chk.substring(0, chk.lastIndexOf(".")) + ".jpg";
+    Name = result[(counter)%4].name;
+    reviewerName.innerText = Name.substring(0, Name.lastIndexOf(".")) + "";
+  }
 }
 
 prev.onclick = async () =>{
@@ -28,4 +43,8 @@ prev.onclick = async () =>{
   }
   // console.log((--counter)%4);
   reviewText.innerText =  (await (await fetch(result[(--counter)%4].download_url)).text());
+  let chk = img_str+result[(counter)%4].name;
+    reviewProfile.src = chk.substring(0, chk.lastIndexOf(".")) + ".jpg";
+    Name = result[(counter)%4].name;
+    reviewerName.innerText = Name.substring(0, Name.lastIndexOf(".")) + "";
 }
