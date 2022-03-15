a0b898263b8ba00d757779d465132f52c4de24c6
Swapnil Patil <patilswapnil01sp.25@gmail.com>
2022-03-15 08:36:27 +0530

    Auto review-text

--- All-Projects/reviews/src/index.js
+++ All-Projects/reviews/src/index.js
@@ -1,10 +1,31 @@
-const url1 = 'https://raw.githubusercontent.com/elkorf/JavaScript-Projects/master/data.txt'
+// const url1 = 'https://raw.githubusercontent.com/elkorf/JavaScript-Projects/master/All-Projects/reviews/review-data/michael-dam.txt'
+const url = 'https://api.github.com/repos/elkorf/JavaScript-Projects/contents/All-Projects/reviews/review-data';
+let reviewText = document.querySelector('#review-txt');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next'); 
+let result="";
+let counter=0;
+
 window.addEventListener("DOMContentLoaded", async function (e) {
-    try {
-      const response = await fetch(url1);
-      const result = await response.text();
-      console.log(result);
-    } catch (err) {
-      return console.log(err);
-    }
-  });
+  try {
+    const response = await fetch(url);
+    result = await response.json();
+    reviewText.innerText =  (await (await fetch(result[0].download_url)).text());;
+  } catch (err) {
+    return console.log(err);
+  }
+});
+
+next.onclick = async () =>{
+  reviewText.innerText =  (await (await fetch(result[(++counter)%4].download_url)).text());
+  // console.log((++counter)%4);
+}
+
+prev.onclick = async () =>{
+  // reviewText.innerText =  (await (await fetch(result[((counter--)+1)%4].download_url)).text());
+  if(((counter)%4)==0){
+    counter=4;
+  }
+  // console.log((--counter)%4);
+  reviewText.innerText =  (await (await fetch(result[(--counter)%4].download_url)).text());
+}
